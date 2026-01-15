e3309b156502e8d84d3f9ce4a380fe184d0b6a57
Rahul <rahul.wa47@gmail.com>
2026-01-15 23:38:18 +0530

    added test to check hit and sunk methods

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,2 +1,17 @@
 import Ship from "../src/modules/ship";
 
+
+it("check damage to the ship", () => {
+    let newShip = Ship(3)
+    newShip.hit()
+    
+    expect(newShip.hitCount()).toEqual(1)
+})
+
+it("check if she is sunk", () => {
+    let newShip = Ship(3)
+    newShip.hit()
+    newShip.hit()
+    newShip.hit()
+    expect(newShip.isSunk()).toBe(true)
+})

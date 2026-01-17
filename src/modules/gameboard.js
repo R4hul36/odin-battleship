8d7dca33ab9c1a88fcf80d2e7a84612516ce6f5a
Rahul <rahul.wa47@gmail.com>
2026-01-17 16:58:17 +0530

    added a method for placing ships on board with out any checks

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,15 +1,24 @@
+import Ship from "./ship"
 
 export default function GameBoard () {
     let board
     function createBoard () {
-        board = Array.from({length : 10}, () => {
-            new Array(10).fill(null)
-        })
-       
+        board = Array.from({length : 10}, () => 
+            Array(10).fill(null)
+        )
     }
 
+    function placeShips (x,y) {
+        let newShip = Ship(1)
+        board[x][y] = newShip
+    }
+
+    
+
     return {
-        createBoard
+        createBoard,
+        placeShips,
+    
     }
 
 } 

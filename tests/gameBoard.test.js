64366632a5ff84a51715d555dd0e1c1ad246b3d2
Rahul <rahul.wa47@gmail.com>
2026-01-17 17:04:18 +0530

    added a test that creates new board, places ships and checks if the correct cell is occupied

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,3 +1,8 @@
 import GameBoard from "../src/modules/gameboard";
 
-
+it("check if a ship is placed on the correct coordinate on the board", () => {
+    let newBoard = GameBoard()
+    newBoard.createBoard()
+    newBoard.placeShips(0,0)
+    expect(newBoard.checkIfOccupied(0,0)).toBe(1)
+})

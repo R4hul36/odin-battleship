d59b37ea0ed329933b5436a4126f2cf40add481f
Rahul <rahul.wa47@gmail.com>
2026-01-21 12:50:09 +0530

    The ship module now also return the length

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,20 +1,23 @@
+export default function Ship(length, count = 0) {
+  let hitCount = count
+  function hit() {
+    hitCount++
+  }
 
+  function isSunk() {
+    return hitCount === length ? true : false
+  }
 
-export default function Ship (length, count=0) {
-    let hitCount = count
-    function hit (){
-        hitCount++
-    }
-    
-    function isSunk() {
-        return hitCount === length? true : false
-    }
+  function shipLength() {
+    return length()
+  }
 
-    return {
-        hitCount(){
-            return hitCount
-        },
-        hit,
-        isSunk  
-    }
+  return {
+    hitCount() {
+      return hitCount
+    },
+    hit,
+    shipLength,
+    isSunk,
+  }
 }

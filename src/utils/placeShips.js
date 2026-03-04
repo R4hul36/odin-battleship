5205e807f8284872b8e6caa18a31dda79d4949ba
Rahul <rahul.wa47@gmail.com>
2026-03-04 17:24:43 +0530

    fixed the intinite loop during auto placement by switching orientation

--- src/utils/placeShips.js
+++ src/utils/placeShips.js
@@ -8,15 +8,26 @@ export function placeShips(player) {
   // const shipSizes = [2, 3, 3, 4, 5]
 
   const fleet = player.getFleet()
-  console.log(fleet)
+  let placementCount = 0
 
   for (let i = 0; i < fleet.length; i++) {
     const ship = fleet[i]
-    console.log(ship.shipInfo())
+    
     let coords = generateRandomCoordinates()
-    const orientation = randomOrientation()
+    let orientation = randomOrientation()
     while (!player.placeShip(ship, coords, orientation)) {
+      
+      if(placementCount > 100) {
+        console.log("exceeded the count, will change orientation now!");
+        
+        orientation = orientation === "horizontal" ? "vertical": "horizontal"
+        placementCount = 0
+        continue
+        
+      }
       coords = generateRandomCoordinates()
+      placementCount++
     }
   }
+  return true
 }
